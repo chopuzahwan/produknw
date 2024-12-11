2581bf1698f1b16c7c92302f6ff47ba83746ea81
chopuzahwan <gelisskalasma11@gmail.com>
2024-12-11 14:10:24 +0700

    Add files via upload

---
+++ lib/models/api.dart
@@ -0,0 +1,7 @@
+class BaseUrl {
+  static String tambah = "http://10.2.17.95/produk/create.php";
+  static String data = "http://10.2.17.95/produk/list.php";
+  // static String lihat = "http://192.168.1.103/produk/details.php";
+  static String edit = "http://10.2.17.95/produk/update.php";
+  static String hapus = "http://10.2.17.95/produk/delete.php";
+}
